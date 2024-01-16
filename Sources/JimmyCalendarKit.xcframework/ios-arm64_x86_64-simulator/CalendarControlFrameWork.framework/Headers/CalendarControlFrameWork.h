0759cf08349eb015a7fac9d98fd6ac667ddaf222
Jimmy Chen <jinny1993820@gmail.com>
2024-01-16 14:12:59 +0800

    Initial Commit

---
+++ Sources/JimmyCalendarKit.xcframework/ios-arm64_x86_64-simulator/CalendarControlFrameWork.framework/Headers/CalendarControlFrameWork.h
@@ -0,0 +1,18 @@
+//
+//  CalendarControlFrameWork.h
+//  CalendarControlFrameWork
+//
+//  Created by YuMing on 2024/1/15.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for CalendarControlFrameWork.
+FOUNDATION_EXPORT double CalendarControlFrameWorkVersionNumber;
+
+//! Project version string for CalendarControlFrameWork.
+FOUNDATION_EXPORT const unsigned char CalendarControlFrameWorkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <CalendarControlFrameWork/PublicHeader.h>
+
+
